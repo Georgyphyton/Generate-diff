7b17864829064d542371f073ae08655d2a1f11e8
Georgy <nov_georgy@inbox.ru>
2023-06-28 17:52:23 +0400

    2 step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,8 @@
-import argparse
-
-parser = argparse.ArgumentParser(
-    description='Compares two configurat files and shows a difference.')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-args = parser.parse_args()
+from gendiff.gendiff import parsik
 
 
 def main():
-    print(args.accumulate(args.integers))
+    parsik()
 
 
 if __name__ == '__main__':
