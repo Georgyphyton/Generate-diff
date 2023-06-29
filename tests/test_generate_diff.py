0053f2b1d2ce2f326c3fcb154ef904559bf6789a
Georgy <nov_georgy@inbox.ru>
2023-06-29 15:53:20 +0400

    fix test_generate

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,7 +1,11 @@
 from gendiff.gendiff import generate_diff
 from tests import pass_to_file
+import pytest
 
 
+@pytest.mark.parametrize('input1,input2,output',
+                         pytest.param(
+                            'file1.json', 'file2.json', 'test_json.txt',))
 def test_json(file1, file2, result_file):
     with open(pass_to_file(result_file)) as result:
         text_result = result.read()
